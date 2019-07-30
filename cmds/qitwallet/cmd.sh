b1e1a6cf8e96544c7423be29ef83d2af73c8873c
Wayman <i5d@live.com>
2019-07-31 00:25:21 +0800

    update rpc wallet create and open

---
+++ cmds/qitwallet/cmd.sh
@@ -0,0 +1,1 @@
+curl -X POST   -H 'Content-Type: application/json' -d '{"jsonrpc":"1.0","method":"account_listAccounts","params":[],"id":1}' -u admin:123  http://127.0.0.1:38130/api
--- wallet/api.go
+++ wallet/api.go
@@ -1,76 +1,35 @@
 package wallet
 
 import (
-	"fmt"
-	"path/filepath"
-
-	log "github.com/sirupsen/logrus"
-
 	"github.com/HalalChain/qitmeer-wallet/config"
-	"github.com/HalalChain/qitmeer-wallet/utils"
+	waddrmgr "github.com/HalalChain/qitmeer-wallet/waddrmgs"
 )
 
-// API wallet
-type API struct {
+// AccountAPI wallet
+type AccountAPI struct {
 	cfg *config.Config
 
 	wt *Wallet
 }
 
-// NewAPI api make
-func NewAPI(cfg *config.Config) *API {
-	return &API{}
-}
-
-// Status wallet info
-func (api *API) Status() (status *ResStatus) {
-	status = &ResStatus{}
-
-	if api.wt == nil {
-		status.Stats = "nil"
-		return
+// NewAccountAPI api make
+func NewAccountAPI(cfg *config.Config) *AccountAPI {
+	return &AccountAPI{
+		cfg: cfg,
 	}
-
-	return nil
 }
 
-//Create wallet
-func (api *API) Create(walletPass string) error {
-	log.Trace("CreateAPI CreateWallet")
-
-	activeNetParams := utils.GetNetParams(api.cfg.Network)
-
-	dbDir := filepath.Join(api.cfg.AppDataDir, activeNetParams.Name)
-	loader := NewLoader(activeNetParams, dbDir, 250)
-
-	walletExist, err := loader.WalletExists()
+// ListAccount
+func (api *AccountAPI) ListAccounts() (map[string]float64, error) {
+	accountBalances := map[string]float64{}
+	results, err := api.wt.AccountBalances(waddrmgr.KeyScopeBIP0044, int32(16))
 	if err != nil {
-		log.Errorf("qitmeer start err: load wallet ", err)
-		return fmt.Errorf("load Wallet err: %s ", err)
+		return nil, err
 	}
-	if !walletExist {
-		err := CreateWallet(api.cfg, walletPass)
-		if err != nil {
-			log.Errorf("qitmeer start err: crate wallet ", err)
-			return fmt.Errorf("crate wallet err: %s", err)
-		}
+
+	for _, result := range results {
+		accountBalances[result.AccountName] = result.AccountBalance.ToCoin()
 	}
-	wt, err := loader.OpenExistingWallet([]byte(walletPass), false)
-	if err != nil {
-		log.Errorf("newWallet err: %s", err)
-		return fmt.Errorf("open wallet err: %s", err)
-	}
-	api.wt = wt
-
-	return nil
-}
-
-//Open wallet
-func (api *API) Open(walletPass string) error {
-	return nil
-}
-
-//ResStatus statusinfo
-type ResStatus struct {
-	Stats string //nil,closed,opened
+	// Return the map.  This will be marshaled into a JSON object.
+	return accountBalances, nil
 }
